d15c7f0606fce60391ef59ca1d035d1946bd3e1d
Danil <danbraz13@gmail.com>
2021-02-03 20:35:24 +0500

    ываыва

---
+++ lesson-3/script.js
@@ -0,0 +1,20 @@
+let money = 26500, 
+  income = "Фриланс",
+  addExpenses = "Аренда квартиры, Интеренет, Подписка на Netflix",
+  deposit = false, 
+  mission = 3500000,
+  period = 6;
+
+console.log(typeof money, income, deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period +  " месяцев. Цель заработать " + mission + " рублей.");
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+let budgetDay = money / 30;
+
+console.log(budgetDay);
+
+let questionMoney = +promt("Ваш месячный доход?");
+
+questionMoney = money;
